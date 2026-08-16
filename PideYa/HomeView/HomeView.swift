b593283780bcc83741acffa7ec5e395d956d05ba
Victor Arana <3av.arana@gmail.com>
2026-08-16 18:27:22 -0400

    Create basic homeView components

---
+++ PideYa/HomeView/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  PideYa
+//
+//  Created by Victor Arana on 16/8/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ PideYa/HomeView/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  PideYa
+//
+//  Created by Victor Arana on 16/8/26.
+//
+
+import Foundation
